1aab868a4185de99841d7c615e6b331674a4b798
Pat B <patrick.bornay@gmail.com>
2023-11-24 21:10:42 +1100

    start main page setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
 import OpeningScreen from './components/openingScreen/OpeningScreen';
 import React from 'react';
+import gsap from 'gsap';
 
+let tl = gsap.timeline();
 
 function App() {
+  const openingScreenWrapperRef = React.useRef();
+
+  React.useLayoutEffect(() => {
+    const selectedRef = gsap.utils.selector(openingScreenWrapperRef.current);
+    let ctx = gsap.context(() => {
+      tl.from(selectedRef, {});
+    });
+    return () => ctx.revert();
+  }, []);
+
   return (
     <>
-      <OpeningScreen />
+      <div
+        className={'app__opening-screen-wrapper'}
+        ref={openingScreenWrapperRef}
+      >
+        <OpeningScreen />
+      </div>
+      <div className={'app__main'}>
+        <h1>Hello world</h1>
+      </div>
     </>
   );
 }
