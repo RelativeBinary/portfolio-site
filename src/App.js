cb0f7b2221e6c5030ca2bacf6926b3e67c498842
Pat B <patrick.bornay@gmail.com>
2023-11-26 00:55:21 +1100

    content updates

--- src/App.js
+++ src/App.js
@@ -28,16 +28,20 @@ function App() {
       </div>
       <div className={'app__main-wrapper'} ref={mainRef}>
         <div className={'main__content'}>
-          <h2 className={'main__heading'}>patrick bornay</h2>
+          <h2 className={'main__heading'}>hi i'm patrick bornay</h2>
           <p>
-            I do stuff on the internet, currently a software engineer working on
-            frontend web app migrations, UI designs, API development and other
-            stuff
+            I do stuff on the internet, apparently i'm a software engineer
+            working on frontend web app migrations, UI designs, API development
+            and other stuff
           </p>
           <p>You can reach me at:</p>
           <li>linkedIn</li>
           <li>insta</li>
           <li>github</li>
+
+          <p>Tech stack:</p>
+          <li>ReactJS with Typescript, testing with Jest and Mirage</li>
+          <li>Grails & Micronaut with Java/Groovy, testing with Spock.</li>
         </div>
       </div>
     </>
