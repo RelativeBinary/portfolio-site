9e1b882cb7757e865801f1682105b771a23121c7
RelativeBinary <patrick.bornay@gmail.com>
2023-11-25 14:29:55 +1100

    Introduce animations on main page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import './App.css';
-import OpeningScreen from './components/openingScreen/OpeningScreen';
-import React from 'react';
-import gsap from 'gsap';
+import "./App.css";
+import OpeningScreen from "./components/openingScreen/OpeningScreen";
+import React from "react";
+import gsap from "gsap";
 
 let tl = gsap.timeline();
 
 function App() {
-  const openingScreenWrapperRef = React.useRef();
+  const mainRef = React.useRef();
 
   React.useLayoutEffect(() => {
-    const selectedRef = gsap.utils.selector(openingScreenWrapperRef.current);
+    const gsapMainRef = gsap.utils.selector(mainRef.current)("div");
     let ctx = gsap.context(() => {
-      tl.from(selectedRef, {});
+      tl.from(gsapMainRef, {
+        ease: "power4.out",
+        opacity: 0,
+        delay: 10,
+        duration: 5,
+      });
     });
     return () => ctx.revert();
   }, []);
 
   return (
     <>
-      <div
-        className={'app__opening-screen-wrapper'}
-        ref={openingScreenWrapperRef}
-      >
+      <div className={"app__opening-screen-wrapper"}>
         <OpeningScreen />
       </div>
-      <div className={'app__main'}>
-        <h1>Hello world</h1>
+      <div className={"app__main-wrapper"} ref={mainRef}>
+        <div>Hello world</div>
+        {/* 
+          probably want the same skewed text from bottom with scramble (scramble once)
+        */}
       </div>
     </>
   );
